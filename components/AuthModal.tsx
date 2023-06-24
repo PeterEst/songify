1adb49e56c67ad48dffa2253012f9d1eb6169088
Peter Estephan <peter-estephan@outlook.com>
2023-06-24 19:26:52 +0300

    Initial Commit - Add Songs Form

---
+++ components/AuthModal.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import React, { useEffect } from "react";
+import Modal from "./Modal";
+import {
+  useSessionContext,
+  useSupabaseClient,
+} from "@supabase/auth-helpers-react";
+import { useRouter } from "next/navigation";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import useAuthModal from "@/hooks/useAuthModal";
+
+const AuthModal = () => {
+  const supabaseClient = useSupabaseClient();
+  const router = useRouter();
+  const { session } = useSessionContext();
+  const { onClose, isOpen } = useAuthModal();
+
+  /**
+   * If the user is logged in, refresh the page and close the modal
+   */
+  useEffect(() => {
+    if (session) {
+      router.refresh();
+      onClose();
+    }
+  }, [session, router, onClose]);
+
+  /**
+   * Close the modal when the user clicks outside of it
+   */
+  const onChange = (open: boolean) => {
+    if (!open) {
+      onClose();
+    }
+  };
+
+  return (
+    <Modal
+      title="Welcome back"
+      description="Login to your account"
+      isOpen={isOpen}
+      onChange={onChange}
+    >
+      <Auth
+        theme="dark"
+        magicLink
+        providers={["github"]}
+        supabaseClient={supabaseClient}
+        appearance={{
+          theme: ThemeSupa,
+          variables: {
+            default: {
+              colors: {
+                brand: "#404040",
+                brandAccent: "#155e75",
+              },
+            },
+          },
+        }}
+      />
+    </Modal>
+  );
+};
+
+export default AuthModal;
